ee9a2ce038097fe5a55af05aa4f5c3e32b0c2849
Debraj Sengupta <theuniqueraj@gmail.com>
2019-12-14 03:53:39 +0530

    3
    3

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+//index
 router.get('/',(req,res)=> res.render('welcome'));
 
+//dashboard
 router.get('/dashboard',(req,res)=> res.render('dashboard'));
+
+
+//Sectors Page
+router.get('/sectors',(req,res)=> res.render('sectors'));
+
+//Training Page
+router.get('/training',(req,res)=> res.render('training'));
+
+//Seller Page
+router.get('/seller-zone',(req,res)=> res.render('seller-zone'));
+
+//E-corner Page
+router.get('/e-corner',(req,res)=> res.render('e-corner'));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,17 +7,6 @@ router.get('/login',(req,res)=> res.render('login'));
 //Register Page
 router.get('/register',(req,res)=> res.render('register'));
 
-//Sectors Page
-router.get('/sectors',(req,res)=> res.render('sectors'));
-
-//Training Page
-router.get('/training',(req,res)=> res.render('training'));
-
-//Seller Page
-router.get('/seller-zone',(req,res)=> res.render('seller-zone'));
-
-//E-corner Page
-router.get('/e-corner',(req,res)=> res.render('e-corner'));
 
 
 module.exports = router;
